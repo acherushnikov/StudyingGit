8082e6ab4680921039b5affdafb712c5b294b45e
Mikhail Shamkhalov <MNShamhalov.SBT@sberbank.ru>
2017-11-07 19:23:00 +0300

    Добавлен контроллер и кнопку

---
+++ SudyingGit/SudyingGit/PainViewController.m
@@ -0,0 +1,13 @@
+//
+//  PainViewController.m
+//  SudyingGit
+//
+//  Created by Mikhail Shamhalov on 07/11/2017.
+//  Copyright © 2017 none. All rights reserved.
+//
+
+#import "PainViewController.h"
+
+@implementation PainViewController
+
+@end
