e8b512d52a4c0d04df360ba22c9e97f5aded7705
mkholkina <m.kholkina95@gmail.com>
2017-11-07 19:28:24 +0300

    Новая кнопочка

--- SudyingGit/SudyingGit/ViewController.m
+++ SudyingGit/SudyingGit/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "ViewControllerT2.h"
 
 @interface ViewController ()
 
@@ -16,7 +17,8 @@
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+	ViewControllerT2 *viewControllerT2 = [ViewControllerT2 new]
+    [self.navigationController pushViewController:viewControllerT2 animated:YES]
 }
 
 
