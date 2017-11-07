340dffe6d3b1b3a1a66773695d0e19660959aa31
bogdanovkv <bogdakos@mail.ru>
2017-11-07 19:27:54 +0300

    Добавлен новый ViewController и кнопка.

--- SudyingGit/SudyingGit/ViewController.m
+++ SudyingGit/SudyingGit/ViewController.m
@@ -7,16 +7,24 @@
 //
 
 #import "ViewController.h"
-
+#import "NewViewController.h"
 @interface ViewController ()
-
+@property(strong, nonatomic) UIButton* button;
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
 	[super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    
+    self.button = [[UIButton alloc] initWithFrame:CGRectMake(20, 20, 50, 25)];
+    self.button.backgroundColor = [UIColor grayColor];
+    [self.view addSubview:self.button];
+    
+    NewViewController* vc = [[NewViewController alloc] init];
+    
+    [self.navigationController pushViewController:vc animated:YES];
+    // Do any additional setup after loading the view, typically from a nib.
 }
 
 
