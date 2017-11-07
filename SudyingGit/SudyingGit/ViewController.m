d1e194a4140d58fb5cd554c11aa2185f8e6c663c
admin <admin@MacBook-Pro-admin.local>
2017-11-07 19:27:53 +0300

    Добавлена кнопка

--- SudyingGit/SudyingGit/ViewController.m
+++ SudyingGit/SudyingGit/ViewController.m
@@ -7,6 +7,8 @@
 //
 
 #import "ViewController.h"
+#import "NewViewController.h"
+
 
 @interface ViewController ()
 
@@ -17,6 +19,8 @@
 - (void)viewDidLoad {
 	[super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    NewViewController *newViewController = [NewViewController new];
+    [self.navigationController pushViewController:newViewController animated:YES];
 }
 
 
